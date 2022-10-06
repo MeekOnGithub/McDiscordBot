95aff7e31a47aa791cb5e292937a05ab13743656
Meek's <89902968+MeekOnGithub@users.noreply.github.com>
2022-10-06 17:21:09 +0200

    Jl'ai juste config

--- config.py
+++ config.py
@@ -1,1 +1,19 @@
+#░█████╗░░█████╗░███╗░░██╗███████╗██╗░██████╗░
+#██╔══██╗██╔══██╗████╗░██║██╔════╝██║██╔════╝░
+#██║░░╚═╝██║░░██║██╔██╗██║█████╗░░██║██║░░██╗░
+#██║░░██╗██║░░██║██║╚████║██╔══╝░░██║██║░░╚██╗
+#╚█████╔╝╚█████╔╝██║░╚███║██║░░░░░██║╚██████╔╝
+#░╚════╝░░╚════╝░╚═╝░░╚══╝╚═╝░░░░░╚═╝░╚═════╝░
 
+
+
+#Token du bot ici, si vous ne savez faire ce que vous devez faire, merci de regarder ce lien : https://discordpy.readthedocs.io/en/stable/discord.html
+token=""
+#Ici, ça sera le nom (Pseudo) du bot in-game
+usernameBot=""
+#La, l'ip du serveur dans la quel le bot va rejoindre
+ip=""
+#Ici le prefix du bot comme + , etc...
+prefix=""
+#L'id du channel dans le quel les messages de bienvenue, aurevoir apparaitrons 
+joinchannel=""
